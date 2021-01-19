d2d77ec0f5e0cee75ff565b7b40f1fea96529028
Tung Vu <tungvu3196@gmail.com>
2021-01-19 09:17:09 +0700

    update subdomain visits problem

--- HashMap/SubdomainCount/Solution.java
+++ HashMap/SubdomainCount/Solution.java
@@ -1,20 +1,43 @@
-class Solution {
-    public List<String> subdomainVisits(String[] cpdomains) {
-        Map<String, Integer> counts = new HashMap();
-        for (String domain: cpdomains) {
-            String[] cpinfo = domain.split("\\s+");
-            String[] frags = cpinfo[1].split("\\.");
-            int count = Integer.valueOf(cpinfo[0]);
-            String cur = "";
-            for (int i = frags.length - 1; i >= 0; --i) {
-                cur = frags[i] + (i < frags.length - 1 ? "." : "") + cur;
-                counts.put(cur, counts.getOrDefault(cur, 0) + count);
-            }
-        }
+/*
+ * Click `Run` to execute the snippet below!
+ */
 
-        List<String> ans = new ArrayList();
-        for (String dom: counts.keySet())
-            ans.add("" + counts.get(dom) + " " + dom);
-        return ans;
+import java.io.*;
+import java.util.*;
+
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+
+class Solution {
+  public static void main(String[] args) {
+    String[] domains = new String[] {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
+    List<String> result = subDomainsVisits(domains);
+    for(String element : result) {
+      System.out.println(element);
     }
+  }
+  
+  public static List<String> subDomainsVisits(String[] domains) {
+    Map<String, Integer> counts = new HashMap<>();
+    for(String domain : domains) {
+      String[] subDomainElements = domain.split("\\s+");
+      String[] domainFrags = subDomainElements[1].split("\\.");
+      int count = Integer.parseInt(subDomainElements[0]);
+      String cur = "";
+      for(int i = domainFrags.length - 1; i >= 0; i--) {
+        cur = domainFrags[i] + (i < domainFrags.length - 1 ? "." : "") + cur;
+        counts.put(cur, counts.getOrDefault(cur, 0) + count);
+      }
+    }
+    
+    List<String> ans = new ArrayList<>();
+    for(String dom: counts.keySet()) {
+      ans.add(counts.get(dom) + " " + dom);
+    }
+    return ans;
+  }
 }
